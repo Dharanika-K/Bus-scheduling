7edf1b58426bad5980d897a90b29ae5298bfc278
Dharanika-K <dharanikakarthikeyan2806@gmail.com>
2025-05-25 17:35:50 +0530

    FIX: added host and port

--- app.py
+++ app.py
@@ -12,10 +12,11 @@ from itsdangerous import URLSafeTimedSerializer
 app = Flask(__name__, template_folder="templates", static_folder="static")
 app.secret_key = "djmadl2025buschedule"  
 
+load_dotenv()
 
-
-client = MongoClient("mongodb+srv://23z317:dharani.28@cluster0.bkcce2w.mongodb.net/")
+client = MongoClient(os.getenv("MONGO_URI"))
 db = client["bus_scheduling"]
+
 drivers_collection = db["drivers"]
 assignments_collection = db["assignments"]
 schedules_collection = db["schedules"]
@@ -653,4 +654,6 @@ def reset_password(token):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from os import environ
+    port = int(environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
